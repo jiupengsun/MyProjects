e375863763af451f48036e53856264f6bf91d311
samparly <samparly@gmail.com>
2015-12-31 22:46:11 +0800

    PageRank using SNAP

--- Comparable/src/main/java/com/samy/ucr/project/Comparable/pageRank/Page.java
+++ Comparable/src/main/java/com/samy/ucr/project/Comparable/pageRank/Page.java
@@ -3,7 +3,7 @@ package com.samy.ucr.project.Comparable.pageRank;
 public class Page implements Comparable<Page> {
 
 	private int id;
-	private double rankValue;
+	private float rankValue;
 
 	public Page(int id) {
 		this.id = id;
@@ -13,18 +13,18 @@ public class Page implements Comparable<Page> {
 		return id;
 	}
 
-	public void setRankValue(double rv) {
+	public void setRankValue(float rv) {
 		rankValue = rv;
 	}
 
-	public double getRankValue() {
+	public float getRankValue() {
 		return rankValue;
 	}
 
 	@Override
 	public int compareTo(Page o) {
 		// TODO Auto-generated method stub
-		double r = o.getRankValue();
+		float r = o.getRankValue();
 		return rankValue < r ? -1 : (rankValue == r ? 0 : 1);
 	}
 
