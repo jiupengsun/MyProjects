72f15772e5e2d1ba3ca981ab542317e75ebbdb21
samparly <samparly@gmail.com>
2016-01-14 00:04:46 +0800

    Leetcode

--- Leetcode/src/com/samy/leetcode/algorithm/ClimbingStairs.java
+++ Leetcode/src/com/samy/leetcode/algorithm/ClimbingStairs.java
@@ -14,9 +14,21 @@ public class ClimbingStairs {
 	public static int climbStairs(int n) {
 		int sum = 1;
 		for (int i = 1; i <= n / 2; ++i) {
-			sum += combination(i + 1, n - 2 * i);
+			sum += combination(n - i, i);
 		}
 		return sum;
+	}
+
+	public static int climbStairsSample(int n) {
+		int step = 1;
+		int prestep = 1;
+		int temp = 0;
+		while (--n > 0) {
+			temp = step;
+			step = step + prestep;
+			prestep = temp;
+		}
+		return step;
 	}
 
 	/**
@@ -30,16 +42,21 @@ public class ClimbingStairs {
 	 * @reference
 	 */
 	private static int combination(int n, int i) {
-		int sum = 1;
-		while (i > 1)
-			sum *= i--;
-		return (int) Math.pow(n, i) / sum;
+		i = Math.min(i, n - i);
+		long x = 1, y = 1;
+		while (i >= 1) {
+			x *= (n - i + 1);
+			y *= i--;
+		}
+		return (int) (x / y);
 	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int n = 35;
-		System.out.println(climbStairs(n));
+		for (int i = 0; i < 5; ++i) {
+			System.out.println(Math.round(Math.random() * 69));
+		}
+		System.out.println(Math.round(Math.random() * 27));
 	}
 
 }
