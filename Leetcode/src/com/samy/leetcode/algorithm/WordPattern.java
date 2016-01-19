1583fafc5869e3280d50244a44bbe33aad24cc6f
samparly <samparly@gmail.com>
2016-01-19 17:03:18 +0800

    Leetcode

---
+++ Leetcode/src/com/samy/leetcode/algorithm/WordPattern.java
@@ -0,0 +1,24 @@
+package com.samy.leetcode.algorithm;
+
+public class WordPattern {
+
+	/**
+	 * 
+	 * @param pattern
+	 * @param str
+	 * @return
+	 * 2016Äê1ÔÂ19ÈÕ
+	 * @author Jiupeng
+	 * @description
+	 * @reference https://leetcode.com/problems/word-pattern/
+	 */
+	public boolean wordPattern(String pattern, String str) {
+
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
