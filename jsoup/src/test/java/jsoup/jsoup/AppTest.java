995b5bb5dc92f198213c62df4cf7492484b1d298
samparly <samparly@gmail.com>
2016-02-23 16:05:20 +0800

    'jsoup'

--- jsoup/src/test/java/jsoup/jsoup/AppTest.java
+++ jsoup/src/test/java/jsoup/jsoup/AppTest.java
@@ -1,38 +1,24 @@
 package jsoup.jsoup;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import java.io.IOException;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.junit.Test;
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
+public class AppTest {
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+	@Test
+	public void test() {
+		try {
+			Document doc = Jsoup.connect("http://www.baidu.com").get();
+			Element body = doc.body();
+			System.out.println(body.html());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
 }
