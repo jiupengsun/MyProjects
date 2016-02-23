a3a2350ed1d33580eb951db2e7d5bb94c7daf9ea
samparly <samparly@gmail.com>
2016-02-23 13:16:30 +0800

    'jsoup'

---
+++ jsoup/src/main/java/jsoup/jsoup/App.java
@@ -0,0 +1,13 @@
+package jsoup.jsoup;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
