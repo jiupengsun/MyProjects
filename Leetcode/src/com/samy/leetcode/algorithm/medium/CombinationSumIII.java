46a7a7e5fcefff630f83591f83daf51603c066a4
samparly <samparly@gmail.com>
2016-02-25 11:16:38 +0800

    'Leetcode'

--- Leetcode/src/com/samy/leetcode/algorithm/medium/CombinationSumIII.java
+++ Leetcode/src/com/samy/leetcode/algorithm/medium/CombinationSumIII.java
@@ -52,38 +52,110 @@ public class CombinationSumIII {
 		return collection;
 	}
 
+	/**
+	 * 
+	 * @param k
+	 * @param n
+	 * @return
+	 * 2016年2月25日
+	 * @author Jiupeng
+	 * @description 18 test cases, 1ms beats 51.49%
+	 * @reference 
+	 * @interpretation
+	 */
 	public List<List<Integer>> combinationSumIII2(int k, int n) {
+		return helper2(k, n, 9);
+	}
+
+	private List<List<Integer>> helper2(int k, int n, int end) {
 		List<List<Integer>> collection = new ArrayList<List<Integer>>();
-		int end = 9;
-		if (k > 0 && k <= end) {
-			int[] sum = new int[k];
-			while (end > 0) {
-				while (k > 0 && n >= (k + 1) * k >> 1) {
-					end = end < n ? end : n;
-					sum[(k--) - 1] = end;
-					n -= end--;
-				}
-				if (k == 0) {
-					List<Integer> list = new ArrayList<Integer>();
-					for (int i : sum)
-						list.add(i);
-					collection.add(list);
-				} else if (k < sum.length) {
-					end = sum[(++k) - 1];
-					n += end--;
-				} else
-					break;
+		int[] sum = new int[k];
+		while (true) {
+			while (k > 0 && n > 0 && end > 0) {
+				end = end < n ? end : n;
+				sum[(k--) - 1] = end;
+				n -= end--;
 			}
+			if (k == 0 && n == 0) {
+				List<Integer> list = new ArrayList<Integer>();
+				for (int i : sum)
+					list.add(i);
+				collection.add(list);
+			}
+			if (++k > sum.length)
+				break;
+			end = sum[k - 1];
+			n += end--;
+		}
+		return collection;
+	}
+
+	private List<List<Integer>> helper2Enhanced(int k, int n, int end) {
+		List<List<Integer>> collection = new ArrayList<List<Integer>>();
+		int[] sum = new int[k];
+		while (true) {
+			//k * end - ((k - 1) * k >> 1): end + end-1 + end-2 +.. + end-(k-1)
+			//(k + 1) * k >> 1: 1+2+3+...+k
+			while (k > 0 && n <= k * end - ((k - 1) * k >> 1)
+					&& n >= (k + 1) * k >> 1) {
+				end = end < n ? end : n;
+				sum[(k--) - 1] = end;
+				n -= end--;
+			}
+			if (k == 0) {
+				List<Integer> list = new ArrayList<Integer>();
+				for (int i : sum)
+					list.add(i);
+				collection.add(list);
+			}
+			if (++k > sum.length)
+				break;
+			end = sum[k - 1];
+			n += end--;
 		}
 
 		return collection;
+	}
+
+	/**
+	 * 
+	 * @param k
+	 * @param n
+	 * @return
+	 * 2016年2月25日
+	 * @author Jiupeng
+	 * @description backTracking algorithm
+	 * @reference 
+	 * @interpretation https://leetcode.com/discuss/44514/simple-java-recursive-solution
+	 */
+	public List<List<Integer>> combinationSumIII3(int k, int n) {
+		List<List<Integer>> res = new ArrayList<List<Integer>>();
+		backtracking(new ArrayList<Integer>(), res, n, 1, k);
+		return res;
+	}
+
+	private void backtracking(List<Integer> temp, List<List<Integer>> res,
+			int tar, int k, int count) {
+		if (tar == 0 && count == 0) {
+			res.add(temp);
+			return;
+		}
+		if (count == 0 || tar <= 0)
+			return;
+
+		for (int i = k; i <= 9; i++) {
+			temp.add(i);
+			backtracking(new ArrayList<Integer>(temp), res, tar - i, i + 1,
+					count - 1);
+			temp.remove(temp.size() - 1);
+		}
 	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int k = 3, n = 9;
 		CombinationSumIII c = new CombinationSumIII();
-		c.combinationSumIII2(k, n);
+		c.combinationSumIII3(k, n);
 	}
 
 }
