f5a2a779a644b667c9a4f23c2fc6df779800cdef
samparly <samparly@gmail.com>
2016-03-11 00:05:02 +0800

    'Leetcode'

--- Leetcode/src/com/samy/leetcode/algorithm/medium/DifferentWaystoAddParentheses.java
+++ Leetcode/src/com/samy/leetcode/algorithm/medium/DifferentWaystoAddParentheses.java
@@ -17,72 +17,58 @@ public class DifferentWaystoAddParentheses {
 	 */
 	public List<Integer> diffWaysToCompute(String input) {
 		List<Integer> list = new ArrayList<Integer>();
-		int i = 0, l = input.length(), n1 = 0;
-		char c = '\0';
-		while (i < l) {
-			//find the first sign
-			c = input.charAt(i);
-			if (c < '0' || c > '9')
-				break;
-			++i;
-		}
-		n1 = Integer.parseInt(input.substring(0, i));
-		calculate(c, n1, diffWaysToCompute(input.substring(i + 1)), list);
-		int j = i + 1, n2 = 0;
-		char c2 = '\0';
-		while (j < l) {
-			//find the second sign
-			c2 = input.charAt(j);
-			if (c2 < '0' || c2 > '9')
-				break;
-			++j;
-		}
-		if (j > i + 1) {
-			switch (c) {
-			case '+':
-				n2 = n1 + Integer.parseInt(input.substring(i + 1, j));
-				break;
-			case '-':
-				n2 = n1 - Integer.parseInt(input.substring(i + 1, j));
-				break;
-			case '*':
-				n2 = n1 * Integer.parseInt(input.substring(i + 1, j));
-				break;
-			default:
+		if (input == null || input.length() == 0)
+			return list;
+		int round = 2;
+		for (int i = 0, l = input.length(); i < l && round > 0; --round, ++i) {
+			while (i < l && input.charAt(i) >= '0' && input.charAt(i) <= '9')
+				++i;
+			if (round == 2 && i == l) {
+				list.add(Integer.parseInt(input));
 				return list;
 			}
-			calculate(c2, n2, diffWaysToCompute(input.substring(i + 1)), list);
+			calculate(input.charAt(i), diffWaysToCompute(input.substring(0, i)), diffWaysToCompute(input.substring(i + 1)),
+					list);
 		}
 		return list;
 	}
 
-	private void calculate(char sign, int left, List<Integer> right,
-			List<Integer> list) {
-		if (right.size() == 0) {
-			list.add(left);
+	private void calculate(char sign, List<Integer> left, List<Integer> right, List<Integer> list) {
+		if (left.size() == 0 && right.size() == 0) {
 			return;
+		} else if (left.size() == 0)
+			list.addAll(right);
+		else if (right.size() == 0)
+			list.addAll(left);
+		else {
+			switch (sign) {
+			case '+':
+				for (int l : left)
+					for (int r : right)
+						list.add(l + r);
+				break;
+			case '-':
+				for (int l : left)
+					for (int r : right)
+						list.add(l - r);
+				break;
+			case '*':
+				for (int l : left)
+					for (int r : right)
+						list.add(l * r);
+				break;
+			default:
+				return;
+			}
 		}
-		switch (sign) {
-		case '+':
-			for (int i : right)
-				list.add(left + i);
-			break;
-		case '-':
-			for (int i : right)
-				list.add(left - i);
-			break;
-		case '*':
-			for (int i : right)
-				list.add(left * i);
-			break;
-		default:
-			return;
-		}
+
 	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		String input = "0+1";
+		DifferentWaystoAddParentheses dwa = new DifferentWaystoAddParentheses();
+		dwa.diffWaysToCompute(input);
 	}
 
 }
