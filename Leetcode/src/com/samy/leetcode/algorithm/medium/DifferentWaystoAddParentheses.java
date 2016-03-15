e1195326cd856ee097cac1da0ed57d3fa3c266c4
samparly <samparly@gmail.com>
2016-03-15 16:59:30 +0800

    'Leetcode'

--- Leetcode/src/com/samy/leetcode/algorithm/medium/DifferentWaystoAddParentheses.java
+++ Leetcode/src/com/samy/leetcode/algorithm/medium/DifferentWaystoAddParentheses.java
@@ -18,6 +18,21 @@ public class DifferentWaystoAddParentheses {
 	 */
 	public List<Integer> diffWaysToCompute(String input) {
 		List<Integer> list = new ArrayList<Integer>();
+		if (input != null && input.length() > 0) {
+			//read the first number
+			int i = 0, l = input.length();
+			while (i < l) {
+				char c = input.charAt(i);
+				if (c < '0' || c > '9')
+					break;
+				++i;
+			}
+			if (i > 0) {
+				int n1 = Integer.parseInt(input.substring(0, i));
+
+				//read the second number
+			}
+		}
 
 		return list;
 	}
@@ -26,14 +41,17 @@ public class DifferentWaystoAddParentheses {
 			Stack<Integer> numbers) {
 		List<Integer> list = new ArrayList<Integer>();
 
-		if (numbers.size() != signs.size() + 1)
-			return list;
-		int n1 = numbers.pop();
-		char c1 = signs.pop();
-		switch (c1) {
-		case '+':
+		if (numbers.size() == 1)
+			list.add(numbers.pop());
+		else if (numbers.size() > 1) {
+			int n1 = numbers.pop();
+			char c1 = signs.pop();
+			List<Integer> subList = calculate(signs, numbers);
+			if (subList.size() > 0) {
 
+			}
 		}
+
 		return list;
 	}
 
