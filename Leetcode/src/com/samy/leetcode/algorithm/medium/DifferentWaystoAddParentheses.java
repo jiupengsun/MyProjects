92688f65b3a553e714f5b96e20c36895b2e6ae0f
samparly <samparly@gmail.com>
2016-03-16 23:00:50 +0800

    'Leetcode'

--- Leetcode/src/com/samy/leetcode/algorithm/medium/DifferentWaystoAddParentheses.java
+++ Leetcode/src/com/samy/leetcode/algorithm/medium/DifferentWaystoAddParentheses.java
@@ -1,9 +1,8 @@
 package com.samy.leetcode.algorithm.medium;
 
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.Queue;
+import java.util.Stack;
 
 public class DifferentWaystoAddParentheses {
 
@@ -18,71 +17,66 @@ public class DifferentWaystoAddParentheses {
 	 * @interpretation
 	 */
 	public List<Integer> diffWaysToCompute(String input) {
-		Queue<Character> signs = new LinkedList<Character>();
-		Queue<Integer> numbers = new LinkedList<Integer>();
-		int i = 0, j = 0, l = input.length();
-		while (j < l) {
+		Stack<Character> signs = new Stack<Character>();
+		Stack<Integer> numbers = new Stack<Integer>();
+		int l = input.length(), i = l, j = l - 1;
+		while (j >= 0) {
 			char c = input.charAt(j);
 			if (c >= '0' && c <= '9')
-				++j;
+				--j;
 			else {
-				numbers.add(Integer.parseInt(input.substring(i, j)));
-				signs.add(c);
-				i = ++j;
+				numbers.push(Integer.parseInt(input.substring(j + 1, i)));
+				signs.push(c);
+				i = j--;
 			}
 		}
-		if (j > i)
-			numbers.add(Integer.parseInt(input.substring(i, j)));
+		if (j < 0)
+			numbers.push(Integer.parseInt(input.substring(0, i)));
 
 		return calculate(signs, numbers);
 	}
 
-	private List<Integer> calculate(Queue<Character> signs,
-			Queue<Integer> numbers) {
+	private List<Integer> calculate(Stack<Character> signs, Stack<Integer> numbers) {
 		List<Integer> list = new ArrayList<Integer>();
 		if (numbers.size() == 1)
 			list.add(numbers.peek());
 		else if (numbers.size() > 1) {
-			int n1 = numbers.poll();
-			char c1 = signs.poll();
-			helper(n1, c1, calculate(signs, numbers), list);
-			int n2 = numbers.poll();
+			int n1 = numbers.pop();
+			char c1 = signs.pop();
+			int n2 = numbers.pop();
 			int n = helper(n1, n2, c1);
+			helper(n1, c1, calculate(signs, numbers), list);
 			if (signs.size() > 0) {
-				char c2 = signs.poll();
+				char c2 = signs.pop();
 				helper(n, c2, calculate(signs, numbers), list);
-				signs.add(c2);
+				signs.push(c2);
 			} else
 				list.add(n);
-			numbers.add(n2);
-			numbers.add(n1);
-			signs.add(c1);
+			numbers.push(n2);
+			numbers.push(n1);
+			signs.push(c1);
 		}
 		return list;
 	}
 
-	private void helper(int n, char c, List<Integer> subList,
-			List<Integer> list) {
-		if (subList.size() == 0)
-			list.add(n);
-		else {
-			switch (c) {
-			case '+':
-				for (int i : subList)
-					list.add(n + i);
-				break;
-			case '-':
-				for (int i : subList)
-					list.add(n - i);
-				break;
-			case '*':
-				for (int i : subList)
-					list.add(n * i);
-				break;
-			default:
-				return;
-			}
+	private void helper(int n, char c, List<Integer> subList, List<Integer> list) {
+		switch (c) {
+		case '+':
+			for (int i : subList)
+				list.add(n + i);
+			break;
+		case '-':
+			for (int i : subList)
+				list.add(n - i);
+			break;
+		case '*':
+			for (int i : subList)
+				list.add(n * i);
+			break;
+		default:
+			return;
 		}
+
 	}
 
 	private int helper(int n1, int n2, char c) {
