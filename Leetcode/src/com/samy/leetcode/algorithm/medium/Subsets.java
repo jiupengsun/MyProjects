95b0daf6f5e2c45bb3ad2ebc70a27cbe57d16531
samparly <samparly@gmail.com>
2016-03-28 17:00:44 +0800

    'Leetcode'

--- Leetcode/src/com/samy/leetcode/algorithm/medium/Subsets.java
+++ Leetcode/src/com/samy/leetcode/algorithm/medium/Subsets.java
@@ -11,16 +11,22 @@ public class Subsets {
 	 * @return
 	 * 2016Äê3ÔÂ27ÈÕ
 	 * @author Jiupeng
-	 * @description
+	 * @description 10 test cases, 3ms beats 15.24%
 	 * @reference https://leetcode.com/problems/subsets/
 	 * @interpretation
 	 */
 	public List<List<Integer>> subsets(int[] nums) {
 		List<List<Integer>> sets = new ArrayList<List<Integer>>();
-		for (int i = 0, l = nums.length; i <= l; ++i) {
-			List<Integer> set = new ArrayList<Integer>();
-			for (int j = i, k; j > 0; --j) {
-
+		sets.add(new ArrayList<Integer>());
+		for (int i : nums) {
+			for (int j = 0, l = sets.size(); j < l; ++j) {
+				List<Integer> newSet = new ArrayList<Integer>();
+				newSet.addAll(sets.get(j));
+				int m = 0, n = newSet.size();
+				while (m < n && newSet.get(m) < i)
+					++m;
+				newSet.add(m, i);
+				sets.add(newSet);
 			}
 		}
 		return sets;
---
+++ Leetcode/src/com/samy/leetcode/algorithm/medium/Triangle.java
@@ -0,0 +1,26 @@
+package com.samy.leetcode.algorithm.medium;
+
+import java.util.List;
+
+public class Triangle {
+
+	/**
+	 * 
+	 * @param triangle
+	 * @return
+	 * 2016Äê3ÔÂ28ÈÕ
+	 * @author Jiupeng
+	 * @description
+	 * @reference https://leetcode.com/problems/triangle/
+	 * @interpretation
+	 */
+	public int minimumTotal(List<List<Integer>> triangle) {
+
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
