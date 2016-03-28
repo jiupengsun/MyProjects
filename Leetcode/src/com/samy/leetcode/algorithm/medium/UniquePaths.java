bcdb975bca00073ec3fbff5d0544312c88e5a881
samparly <samparly@gmail.com>
2016-03-28 21:55:21 +0800

    'Leetcode'

--- Leetcode/src/com/samy/leetcode/algorithm/medium/UniquePaths.java
+++ Leetcode/src/com/samy/leetcode/algorithm/medium/UniquePaths.java
@@ -46,7 +46,21 @@ public class UniquePaths {
 	 * @return
 	 * 2016Äê2ÔÂ22ÈÕ
 	 * @author Jiupeng
-	 * @description
+	 * @description first, image you have a m*n matrix, and you start from the left-top corner, to the right-bottom corner
+	 * and at the beginning, the matrix[0][0] would be 1, means you only have one way to reach the position where you are
+	 * and then if you want to move to the end of the first row, which is matrix[0][n-1], you only have one way to there, too
+	 * you just need to move along the first row, to the rightmost position. Thus, you may set all values in the first row to 1
+	 * and you move down, if you want to reach the bottom of the leftmost column. Similarly, you only have one way to be there, too
+	 * thus you may set all values in the leftmost column to be one.
+	 * Let's go further, if you want to reach the right end of second row, how many different paths you can choose?
+	 * Let's say, if you want to get matrix[1][1], you can either move right firstly, then move down, or you can move down firstly, 
+	 * then move right, so you will have two paths to be the position(1,1), and matrix[1][1] will equal 2. So how can we can this number?
+	 * Obviously, we could know matrix[i][j] = matrix[i-1][j] + matrix[i][j-1], because if we can get the position (i-1,j) or (i, j-1),
+	 * then we only have one way to be the destination -- move down or move right --, thus we use this formula matrix[i][j] = matrix[i-1][j] + matrix[i][j-1]
+	 * to fill up all of the values in the matrix.
+	 * Further more, do we really need o(m*n) space to help us finishing this task? Not really, through filling the matrix, we may see
+	 * the filling task could be fulfilled layer by layer. Another saying, we could use only 1-dimension row array to present each row 
+	 * of the matrix. And we fill the array again and again till n times (0,1,2...n-1). And finally we will get the result we want.
 	 * @reference 
 	 * @interpretation https://leetcode.com/discuss/70352/6-lines-java-0ms-solution
 	 */
