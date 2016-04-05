c16b74e943348f4aa15c897d56ab1a52301dcc99
samparly <samparly@gmail.com>
2016-04-05 18:05:27 +0800

    'Leetcode'

--- Leetcode/src/com/samy/leetcode/algorithm/medium/_3Sum.java
+++ Leetcode/src/com/samy/leetcode/algorithm/medium/_3Sum.java
@@ -19,35 +19,46 @@ public class _3Sum {
 	public List<List<Integer>> threeSum(int[] nums) {
 		List<List<Integer>> sumList = new ArrayList<List<Integer>>();
 		Arrays.sort(nums);
-		for (int i = 0, l = nums.length; i < l - 3; ++i) {
-			int st = i + 1, en = l - 1;
-			for (; st < en;) {
-				int sum = nums[i] + nums[st] + nums[en];
-				if (sum == 0) {
-					if (sumList.size() > 0) {
-						List<Integer> lastList = sumList.get(sumList.size() - 1);
-						if (nums[i] == lastList.get(0) && nums[st] == lastList.get(1)
-								&& nums[en] == lastList.get(2))
-							continue;
-					}
-					List<Integer> list = new ArrayList<Integer>();
-					list.add(nums[i]);
-					list.add(nums[st]);
-					list.add(nums[en]);
-					sumList.add(list);
-					--en;
-				} else if (sum > 0)
-					--en;
-				else
-					++st;
-			}
+		int l = nums.length, last = Integer.MAX_VALUE;
+		for (int i = 0; i < l - 2; ++i) {
+			if (nums[i] == last)
+				continue;
+			last = nums[i];
+			twoSum(sumList, nums, -nums[i], i + 1, l - 1);
 		}
+
 		return sumList;
+	}
+
+	private void twoSum(List<List<Integer>> sumList, int[] nums, int target,
+			int st, int en) {
+		int last = Integer.MAX_VALUE;
+		while (st < en) {
+			if (nums[st] == last) {
+				++st;
+				continue;
+			}
+			last = nums[st];
+			int sum = nums[st] + nums[en];
+			if (sum == target) {
+				List<Integer> list = new ArrayList<Integer>();
+				list.add(target);
+				list.add(nums[st]);
+				list.add(nums[en]);
+				sumList.add(list);
+				++st;
+			} else if (sum > target)
+				--en;
+			else
+				++st;
+		}
 	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int[] sum = new int[] { -1, 0, 1, 2, -1, -4 };
+		_3Sum three = new _3Sum();
+		three.threeSum(sum);
 	}
 
 }
