29846418f41123bc536ae82b3e82c4116b321580
samparly <samparly@gmail.com>
2016-04-10 01:51:23 +0800

    'Leetcode'

--- Leetcode/src/com/samy/leetcode/algorithm/medium/CourseSchedule.java
+++ Leetcode/src/com/samy/leetcode/algorithm/medium/CourseSchedule.java
@@ -1,5 +1,10 @@
 package com.samy.leetcode.algorithm.medium;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 public class CourseSchedule {
 
 	/**
@@ -14,7 +19,31 @@ public class CourseSchedule {
 	 * @interpretation
 	 */
 	public boolean canFinish(int numCourses, int[][] prerequisites) {
+		boolean[] visited = new boolean[numCourses];
+		Map<Integer, Set<Integer>> edges = generateEdges(numCourses, prerequisites);
+		for (int i = 0; i < numCourses; ++i)
+			if (!visited[i] && DFS(numCourses, visited, i, i, edges))
+				return true;
+		return false;
+	}
 
+	private boolean DFS(int numCourses, boolean[] visited, int from, int now, Map<Integer, Set<Integer>> edges) {
+		Set<Integer> links = edges.get(now);
+		if(links == null)
+			
+	}
+
+	private Map<Integer, Set<Integer>> generateEdges(int numCourse, int[][] prerequisites) {
+		Map<Integer, Set<Integer>> edges = new HashMap<Integer, Set<Integer>>();
+		for (int i = 0, e = prerequisites.length; i < e; ++i) {
+			Set<Integer> dependent = edges.get(prerequisites[i][0]);
+			if (dependent == null)
+				dependent = new HashSet<Integer>();
+			dependent.add(prerequisites[i][1]);
+			edges.put(prerequisites[i][0], dependent);
+		}
+
+		return edges;
 	}
 
 	public static void main(String[] args) {
