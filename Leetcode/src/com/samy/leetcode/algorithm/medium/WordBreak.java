f51981ad4d088df5e5b6189db0dc7efc98739b3e
samparly <samparly@gmail.com>
2016-04-15 00:03:25 +0800

    'Leetcode'

--- Leetcode/src/com/samy/leetcode/algorithm/medium/WordBreak.java
+++ Leetcode/src/com/samy/leetcode/algorithm/medium/WordBreak.java
@@ -1,5 +1,9 @@
 package com.samy.leetcode.algorithm.medium;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Map;
 import java.util.Set;
 
 public class WordBreak {
@@ -16,19 +20,40 @@ public class WordBreak {
 	 * @interpretation
 	 */
 	public boolean wordBreak(String s, Set<String> wordDict) {
-		List<Integer[]> pairs = new ArrayList<Integer[]>();
-		int l = s.length();
+		Map<Integer, Set<Integer>> seq = new HashMap<Integer, Set<Integer>>();
 		Iterator<String> words = wordDict.iterator();
-		while(words.hasNext()){
+		// match the string with each word
+		// if matched, then put the start and end index in the map
+		while (words.hasNext()) {
 			String word = words.next();
-			int i=-1,j=-1;
-			i = s.indexOf(word);
-			j = i+word.length();
-			for(int p=0, q=pairs.size(); p<q; ++p){
-				int[] pair = pairs.get(p);
-
+			int l = word.length();
+			//greedy search
+			int i = 0;
+			while (true) {
+				i = s.indexOf(word, i);
+				if (i == -1)
+					break;
+				Set<Integer> set = seq.get(i);
+				if (set == null)
+					set = new HashSet<Integer>();
+				set.add(i + l);
+				seq.put(i, set);
+				i += l;
 			}
 		}
+
+		return couldBreak(seq, 0, s.length());
+	}
+
+	private boolean couldBreak(Map<Integer, Set<Integer>> seq, int start, int end) {
+		Set<Integer> set = seq.get(start);
+		if (set == null)
+			return false;
+		for (int index : set) {
+			if (index == end || couldBreak(seq, index, end))
+				return true;
+		}
+		return false;
 	}
 
 	public static void main(String[] args) {
