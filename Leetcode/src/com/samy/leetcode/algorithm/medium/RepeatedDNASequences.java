04b96961cfc329174a832db17954a91bad2cf83f
= <=>
2016-05-05 13:39:43 +0800

    Leetcode

--- Leetcode/src/com/samy/leetcode/algorithm/medium/RepeatedDNASequences.java
+++ Leetcode/src/com/samy/leetcode/algorithm/medium/RepeatedDNASequences.java
@@ -27,6 +27,52 @@ public class RepeatedDNASequences {
 			for (int j = 0; j < 10; ++j) {
 				char c = t.charAt(j);
 				switch (c) {
+					case 'A':
+						num |= 0;
+						break;
+					case 'C':
+						num |= 1;
+						break;
+					case 'G':
+						num |= 2;
+						break;
+					case 'T':
+						num |= 3;
+						break;
+				}
+				num <<= 2;
+			}
+			Integer n = map.get(num);
+			if (n == null)
+				n = 0;
+			if (n == 1)
+				list.add(t);
+			map.put(num, n + 1);
+		}
+		return list;
+	}
+
+	/**
+	 * 
+	 * @param s
+	 * @return
+	 * Apr 22, 2016
+	 * @author Jiupeng
+	 * @description 30 test cases, 48ms beats 35.25%
+	 * @reference 
+	 * @interpretation 
+	 */
+	public List<String> findRepeatedDnaSequences2(String s) {
+		List<String> list = new ArrayList<String>();
+		Map<Integer, Integer> map = new HashMap<Integer,Integer>();
+		int l = s.length(), i = 0;
+		if(l <= 10)
+			return list;
+		String t = s.substring(0, 10);
+		int num = 0;
+		for(; i<10; ++i){
+			char c = t.charAt(i);
+			switch (c) {
 				case 'A':
 					num |= 0;
 					break;
@@ -39,14 +85,33 @@ public class RepeatedDNASequences {
 				case 'T':
 					num |= 3;
 					break;
-				}
-				num <<= 2;
+			}
+			num <<= 2;
+		}
+		num >>= 2;
+		map.put(num, 1);
+		for(i=10; i<l; ++i){
+			num = (num << 2) & 0xFFFFF;
+			char c = s.charAt(i);
+			switch (c) {
+				case 'A':
+					num |= 0;
+					break;
+				case 'C':
+					num |= 1;
+					break;
+				case 'G':
+					num |= 2;
+					break;
+				case 'T':
+					num |= 3;
+					break;
 			}
 			Integer n = map.get(num);
-			if (n == null)
+			if(n == null)
 				n = 0;
-			if (n == 1)
-				list.add(t);
+			else if(n == 1)
+				list.add(s.substring(i-9, i+1));
 			map.put(num, n + 1);
 		}
 		return list;
