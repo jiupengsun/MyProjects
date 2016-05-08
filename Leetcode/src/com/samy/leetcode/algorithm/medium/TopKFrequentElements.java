6c8257b5e5de6c635a8301591b598398b7e2563c
samparly <samparly@gmail.com>
2016-05-08 16:24:03 +0800

    Leetcode

---
+++ Leetcode/src/com/samy/leetcode/algorithm/medium/TopKFrequentElements.java
@@ -0,0 +1,27 @@
+package com.samy.leetcode.algorithm.medium;
+
+import java.util.List;
+
+public class TopKFrequentElements {
+
+	/**
+	 * 
+	 * @param nums
+	 * @param k
+	 * @return 
+	 * Apr 18, 2016
+	 * @author Jiupeng
+	 * @description 
+	 * @reference https://leetcode.com/problems/top-k-frequent-elements/
+	 * @interpretation 
+	 */
+	public List<Integer> topKFrequent(int[] nums, int k) {
+		return null;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
