715961f7bd797b7fb86c55b91d3b5ed9ae573db8
samparly <samparly@gmail.com>
2016-05-11 11:42:34 +0800

    MyProjects

--- CrawlerCMU/src/main/java/com/samy/crawler/com/samy/crawler/connect/Analysis.java
+++ CrawlerCMU/src/main/java/com/samy/crawler/com/samy/crawler/connect/Analysis.java
@@ -1,13 +1,19 @@
 package com.samy.crawler.com.samy.crawler.connect;
 
 import com.samy.crawler.CourseChapter;
+import com.samy.crawler.Runner;
 import org.jsoup.*;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by samy on 16-5-11.
@@ -32,31 +38,29 @@ public class Analysis {
       Elements children = ele.children();
       if (children.size() == 0)
         continue;
-      boolean flag = false;
+/*      boolean flag = false;
       for (Element child : children) {
         // <a href>
         if (child.tagName().equals("a")) {
           flag = true;
           break;
         }
+      }*/
+      String innerH = ele.html().trim();
+      CourseChapter chapter = new CourseChapter();
+      // such as "Overview (...."
+      chapter.setChapterName(innerH.substring(0, innerH.indexOf("(")));
+      Elements aChildren = ele.getElementsByTag("a");
+      for (Element aChild : aChildren) {
+        // the number of a elements is either 1 or 2
+        String href = aChild.attr("href").trim();
+        if (href.contains(".pdf"))
+          chapter.setPdfAddr(href);
+        else
+          chapter.setVideoAddr(jumpToVideo(href));
       }
-      if (flag) {
-        // find tag
-        String innerH = ele.html().trim();
-        CourseChapter chapter = new CourseChapter();
-        // such as "Overview (...."
-        chapter.setChapterName(innerH.substring(0, innerH.indexOf("(")));
-        Elements aChildren = ele.getElementsByTag("a");
-        for (Element aChild : aChildren) {
-          // the number of a elements is either 1 or 2
-          String href = aChild.attr("href");
-          if (href.contains(".pdf"))
-            chapter.setPdfAddr(href);
-          else
-            chapter.setVideoAddr(jumpToVideo(href));
-        }
-        list.add(chapter);
-      }
+      System.out.println("Add course " + chapter.getChapterName());
+      list.add(chapter);
     }
 
   }
@@ -69,8 +73,27 @@ public class Analysis {
    * @return
    */
   private static String jumpToVideo(String href) {
-    Connection conn = Jsoup.connect(href);
-    conn.followRedirects(true);
-    return null;
+    Document doc = Connect.getDocument(href, Connect.timeOut, Connect.maxTryTimes);
+    if (doc == null)
+      return "";
+    String regexString = "https://.*?\\.mp4";
+    String head = doc.head().toString();
+    Pattern p = Pattern.compile(regexString);
+    Matcher m = p.matcher(head);
+    if (m.find()) {
+      String nextHref = m.group();
+      Connection conn = Jsoup.connect(nextHref);
+      conn.followRedirects(true);
+      conn.timeout(Connect.timeOut);
+      try {
+        Document docJ = Connect.getDocument(conn, Connect.maxTryTimes);
+      } catch (UnsupportedMimeTypeException e) {
+        return e.getUrl();
+      }
+
+    }
+    return "";
   }
+
+
 }
