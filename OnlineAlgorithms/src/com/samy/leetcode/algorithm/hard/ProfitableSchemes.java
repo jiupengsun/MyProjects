73737482d56fa27cb7f08fde8ef655f97dc99fbd
Jiupeng Sun <jiusun@jiusun-mn2.linkedin.biz>
2019-10-16 19:04:32 -0700

    not done

--- OnlineAlgorithms/src/com/samy/leetcode/algorithm/hard/ProfitableSchemes.java
+++ OnlineAlgorithms/src/com/samy/leetcode/algorithm/hard/ProfitableSchemes.java
@@ -10,21 +10,22 @@ public class ProfitableSchemes {
    * i, j, k represents G, P, the index of group/profit, so we have
    * C(i, j, k) equals:
    *  1) if (group[k] > i) C(i, j, k+1) since we can't take item k
-   *  2) else C(i, j, k+1)+C(i-group[k], Math.max(0, j-profit[k], k+1))
-   * when j=0, C(;j;index-1) = 1
+   *  2) else C(i, j, k+1) + C(i-group[k], Math.max(0, j-profit[k]), k+1)
    */
   public int profitableSchemes(int G, int P, int[] group, int[] profit) {
     int length = group.length;
     int[][][] dp = new int[G][P][length];
     // initialization for the last element
     for (int i=0, k=length-1; i<G; ++i) {
-      for (int j=0; j<P; ++j) {
-        // suppose we have only one item, then
-        // if profit is zero, OR
-        // the group of this item is smaller than or equal to G AND
-        // the profit of this item is larger than or equal to P
-        // then we can take it
-        if (j == 0 || group[k] <= i && profit[k] >= j) {
+      // when target profit is 0, no matter if we take the item we can meet the requirement,
+      // so we have 2 schemas when group[i] <= target group, otherwise 1
+      dp[i][0][k] = group[k] <= i ? 2 : 1;
+      for (int j=1; j<P; ++j) {
+        // suppose we have only one item, then if
+        // the group[item] is smaller than or equal to G AND
+        // the profit[item] is larger than or equal to P
+        // we can take it
+        if (group[k] <= i && profit[k] >= j) {
           dp[i][j][k] = 1;
         }
       }
@@ -40,8 +41,8 @@ public class ProfitableSchemes {
         }
       }
     }
-    // dp[G-1][P-1][0] is the final result
-    return dp[G-1][P-1][0] % 1000000007;
+    // dp[G-1][0][0] is the final result
+    return dp[G-1][0][0] % 1000000007;
   }
 
   public static void main(String[] args) {
